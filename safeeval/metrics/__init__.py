85c0f6c69a88cd4a59def53c2857fcdc8c9bbe40
mayank choudhary <mayanknst2028@gmail.com>
2026-05-02 11:25:00 +0530

    add metric aggregation utilities

--- safeeval/metrics/__init__.py
+++ safeeval/metrics/__init__.py
@@ -1,3 +1,4 @@
+# Metric aggregation utilities
 from .safety import SafetyMetrics, compute_safety_metrics
 from .capability import CapabilityMetrics, compute_capability_metrics
 __all__ = ["SafetyMetrics", "compute_safety_metrics", "CapabilityMetrics", "compute_capability_metrics"]
